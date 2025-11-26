7743d6ece699876a4a96ca6e99c541a8d407c7d4
atharvky7 <atharvky7@gmail.com>
2025-11-26 19:31:26 +0530

    Add files via upload

---
+++ imem.v
@@ -0,0 +1,3 @@
+// PLACEHOLDER FILE
+// The full Verilog code is too large to embed entirely in this ZIP due to output size limits.
+// Please paste the complete code provided in the ChatGPT messages into this file.
